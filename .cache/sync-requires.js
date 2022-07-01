9631f323111154304d52dfb6a425345340028d90
Shane Keney <shane@spikeball.com>
2022-06-30 23:42:59 -0400

    chore: remove public dir

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/shanekeney/workspace/personal-portfolio/shanekeney-portfolio/node_modules/gatsby-plugin-offline/app-shell.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/shanekeney/workspace/personal-portfolio/shanekeney-portfolio/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/shanekeney/workspace/personal-portfolio/shanekeney-portfolio/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/shanekeney/workspace/personal-portfolio/shanekeney-portfolio/src/pages/index.js")))
 }
